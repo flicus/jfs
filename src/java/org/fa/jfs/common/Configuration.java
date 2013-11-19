78e0c8844ef3a7a7ab8d0b1408b78fbb084d8a5d
flicus <flicus@gmail.com>
2013-11-19 20:35:06 +0400

    Configuration init version

---
+++ src/java/org/fa/jfs/common/Configuration.java
@@ -0,0 +1,79 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2013  Sergey Skoptsov (flicus@gmail.com), Alexey Marin (asmadews@gmail.com)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+ * TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package org.fa.jfs.common;
+
+public class Configuration {
+
+    private String xmppServer;
+    private String xmppLogin;
+    private String xmppPassword;
+    private String xmppResource;
+
+    public Configuration() {
+    }
+
+    public Configuration(String xmppServer, String xmppLogin, String xmppPassword, String xmppResource) {
+        this.xmppServer = xmppServer;
+        this.xmppLogin = xmppLogin;
+        this.xmppPassword = xmppPassword;
+        this.xmppResource = xmppResource;
+    }
+
+    public String getXmppServer() {
+        return xmppServer;
+    }
+
+    public void setXmppServer(String xmppServer) {
+        this.xmppServer = xmppServer;
+    }
+
+    public String getXmppLogin() {
+        return xmppLogin;
+    }
+
+    public void setXmppLogin(String xmppLogin) {
+        this.xmppLogin = xmppLogin;
+    }
+
+    public String getXmppPassword() {
+        return xmppPassword;
+    }
+
+    public void setXmppPassword(String xmppPassword) {
+        this.xmppPassword = xmppPassword;
+    }
+
+    public String getXmppResource() {
+        return xmppResource;
+    }
+
+    public void setXmppResource(String xmppResource) {
+        this.xmppResource = xmppResource;
+    }
+
+    @Override
+    public String toString() {
+        return "Configuration{" +
+                "xmppServer='" + xmppServer + '\'' +
+                ", xmppLogin='" + xmppLogin + '\'' +
+                ", xmppPassword='" + xmppPassword + '\'' +
+                ", xmppResource='" + xmppResource + '\'' +
+                '}';
+    }
+}
---
+++ src/java/org/fa/jfs/common/ConfigurationFactory.java
@@ -0,0 +1,83 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2013  Sergey Skoptsov (flicus@gmail.com), Alexey Marin (asmadews@gmail.com)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+ * TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package org.fa.jfs.common;
+
+import org.apache.log4j.Logger;
+
+public class ConfigurationFactory {
+
+    public static final Logger log = Logger.getLogger(ConfigurationFactory.class);
+    public static final String CONFIGURATION_READER = "org.fa.jfs.configuration.reader";
+    public static final String CONFIGURATION_PATH = "org.fa.jfs.configuration.path";
+
+    private Configuration configuration;
+    private ConfigurationReader reader;
+
+
+    public static ConfigurationFactory getInstance() {
+        return Singleton.instance;
+    }
+
+    private static final class Singleton {
+        final static ConfigurationFactory instance = new ConfigurationFactory();
+    }
+
+    public ConfigurationFactory() {
+    }
+
+    public ConfigurationFactory setReader(ConfigurationReader reader) {
+        this.reader = reader;
+        return this;
+    }
+
+    public Configuration getConfiguration(String filePath) {
+        if (configuration == null) {
+            if (reader == null) reader = getDefaultReader();
+            configuration = reader.read(filePath);
+        }
+        return configuration;
+    }
+
+    public Configuration getConfiguration() {
+        String configurationPath = System.getProperty(CONFIGURATION_PATH);
+        if (configurationPath == null) configurationPath = "configuration.xml";
+        return getConfiguration(configurationPath);
+    }
+
+    private ConfigurationReader getDefaultReader() {
+        ConfigurationReader reader = null;
+        String readerClassName = System.getProperty(CONFIGURATION_READER);
+        if (readerClassName == null)
+            readerClassName = "org.fa.jfs.common.XStreamConfigurationReader";
+        try {
+            Class<? extends ConfigurationReader> readerClass = (Class<ConfigurationReader>) Class.forName(readerClassName);
+            reader = readerClass.newInstance();
+        } catch (ClassNotFoundException e) {
+            log.error(e);
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        } catch (InstantiationException e) {
+            log.error(e);
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        } catch (IllegalAccessException e) {
+            log.error(e);
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+        return reader;
+    }
+}
